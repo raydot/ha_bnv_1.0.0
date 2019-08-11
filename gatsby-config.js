8f3013f87a4b104c3246d028aa076d1f3da7acc1
Haydn Adams <haydn@beyondpapavalley.com>
2019-08-11 12:34:09 -0700

    updated css and stripe

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,14 +6,5 @@ module.exports = {
   siteMetadata: {
     title: "Beyond Napa Valley",
   },
-  plugins: [
-    {
-      resolve: `gatsby-source-stripe`,
-      options: {
-        objects: ['Balance', 'BalanceTransaction', 'Product', 'ApplicationFee', 'Sku', 'Subscription'],
-        secretKey: 'sk_live_0XREXIYoe9TMYkg1Cf78HWkh005OtIT5go',
-        downloadFiles: true,
-      }
-    }
-  ]
+  
 }
